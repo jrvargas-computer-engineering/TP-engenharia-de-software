868a1a7a7721148385271969d2a56c7752256cc7
Mauroh <mauroh@me.com>
2024-07-09 16:23:57 -0300

    brota uma migalha de front-end

--- guia-de/src/App.js
+++ guia-de/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+function SearchBar(){
+  return(
+    <label className='App-search-component'>
+      <td>&nbsp;&#x1F50E;&#xFE0E;&nbsp;</td>
+      <input className='App-search-box' type="text" placeholder="Pesquisar"/>
+    </label>
+  )
+
+}
+
+function SideIcon(){
+  return(
+    <div className='App-side-bar'>
+      <img className='App-side-icon' src='img/account.png' alt='Minha Conta'/>
+      <img className='App-side-icon' src='img/add.png' alt='Adicionar ...'/>
+    </div>
+  )
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SideIcon/>
+        <p className="App-title">GUIA DE</p>
+        <SearchBar/>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
