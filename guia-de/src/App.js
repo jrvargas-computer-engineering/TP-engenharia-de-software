3b7e4fe711796480b9d98a9b06f4043c2751ab9e
Mauroh <mauroh@me.com>
2024-07-09 18:49:02 -0300

    chore: Update margin and button style in App.css, add search functionality in App.js

--- guia-de/src/App.js
+++ guia-de/src/App.js
@@ -1,14 +1,69 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+
+const professions = [
+  "Engenheiros", 
+  "Médicos", 
+  "Professores", 
+  "Advogados", 
+  "Programadores", 
+  "Designers", 
+  "Arquitetos", 
+  "Enfermeiros", 
+  "Farmacêuticos", 
+  "Dentistas",
+  "Psicólogos",
+  "Fisioterapeutas",
+  "Nutricionistas",
+  "Veterinários",
+  "Biólogos",
+  "Químicos",
+  "Físicos",
+  "Astrônomos",
+  "Geólogos",
+  "Meteorologistas"
+];
+
+
+const AnimatedText = ({list, delay}) =>{
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex === list.length) setCurrentIndex(0);
+    if (currentIndex < list.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText(list[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+  
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, delay, list]);
+
+  return <span>{currentText}</span>
+}
+
+function SearchButton(){
+  return(
+    <button className='App-button' type='button'>Pesquisar</button>
+  )
+}
 
 function SearchBar(){
   return(
-    <label className='App-search-component'>
-      <td>&nbsp;&#x1F50E;&#xFE0E;&nbsp;</td>
-      <input className='App-search-box' type="text" placeholder="Pesquisar"/>
-    </label>
+    <>
+      <label className='App-search-component'>
+        <code>&nbsp;&#x1F50E;&#xFE0E;&nbsp;</code>
+        <input className='App-search-box' placeholder='Pesquisar'/>
+      </label>
+      <SearchButton />
+    </>
   )
-
 }
+
+
 
 function SideIcon(){
   return(
@@ -17,7 +72,6 @@ function SideIcon(){
       <img className='App-side-icon' src='img/add.png' alt='Adicionar ...'/>
     </div>
   )
-
 }
 
 function App() {
@@ -25,7 +79,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <SideIcon/>
-        <p className="App-title">GUIA DE</p>
+        <p className="App-title">GUIA DE<code>&nbsp;</code><AnimatedText list={professions} delay={1500} /></p>
         <SearchBar/>
       </header>
     </div>
