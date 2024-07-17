5ed81156fb8921ac627002effedd3a436d4c83ee
Mauroh <mauroh@me.com>
2024-07-17 01:41:36 -0300

    E com profissoes

---
+++ guia-de/src/professions.js
@@ -0,0 +1,22 @@
+export const professions = [
+  "Engenheiros",
+  "Médicos",
+  "Professores",
+  "Advogados",
+  "Programadores",
+  "Designers",
+  "Arquitetos",
+  "Enfermeiros",
+  "Farmacêuticos",
+  "Dentistas",
+  "Psicólogos",
+  "Fisioterapeutas",
+  "Nutricionistas",
+  "Veterinários",
+  "Biólogos",
+  "Químicos",
+  "Físicos",
+  "Astrônomos",
+  "Geólogos",
+  "Meteorologistas"
+];
