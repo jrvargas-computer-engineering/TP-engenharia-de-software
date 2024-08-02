106d6074c4e0601066abbc4a1a96a77466645dd4
jpcorso <jp.corso123@gmail.com>
2024-08-01 21:57:44 -0300

    :white_check_mark: integration test user - user_repository

---
+++ python-server/tests/integration/create_user_test.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+from models.user import User 
+from repositories.user_repository import UserRepository    
+
+from uuid import uuid4  
+import pytest
+
+
+def test_create_user():
+
+    user = User("jp", str(uuid4()), "jp.corso123@gmail.com")
+    user_repository = UserRepository()
+    user_repository.save_user(user)
+
+    saved_user = user_repository.get_user(user.id)
+
+    assert saved_user is not None
+    assert saved_user["username"] == user.username
+    assert saved_user["id"] == user.id
+    assert saved_user["email"] == user.email
+    assert saved_user == user.__dict__
+
+    user_repository.delete_user(user.id)
+
+
+
+user = User("jp", str(uuid4()), "jp")
+
+print(user.get_id())
