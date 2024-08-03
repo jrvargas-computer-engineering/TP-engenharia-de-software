a6f5996cf4e587411f4ebee5b6a305f3ceb91f17
jpcorso <jp.corso123@gmail.com>
2024-08-02 23:58:17 -0300

    settings update

--- python-server/src/settings/settings.py
+++ python-server/src/settings/settings.py
@@ -1,12 +1,19 @@
-
 import os
+import sys
 from dotenv import load_dotenv
 
-env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+
+sys.path.append("../python-server/")
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+env_path = os.path.join(base_dir, '.env')
+
+
+if not load_dotenv(dotenv_path=env_path, override=True):
+    print(f"Erro ao carregar o arquivo .env a partir do caminho: {env_path}")
 
 try:
-    load_dotenv(env_path, override=True)
     MONGO_URI = os.environ.get("MONGO_URI")
+except Exception as e:
+    print(f"Erro ao carregar as variáveis de ambiente: {e}")
 
-except:
-    print("Error loading .env file")
