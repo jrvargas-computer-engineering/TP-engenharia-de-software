7be4213d41599bd9ccade78589a2b16a1e57e92d
João Pedro Licks Corso <92764827+jpcorso@users.noreply.github.com>
2024-08-10 00:12:07 -0300

    :sparkles: Refact api interface and create topic interface (#10)
    * :recycle: refat aplication interface
    
    * :sparkles: topic interface

--- python-server/src/interfaces/routers/guide.py
+++ python-server/src/interfaces/routers/guide.py
@@ -1,13 +1,12 @@
+import sys
+sys.path.append('../../')
 
-
-from fastapi import FastAPI
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from fastapi.exceptions import HTTPException
-
 from usecases import save_guide_usecase
 from infra.repository.guide_repository import GuideRepository
 
-app = FastAPI()
+guide_router = APIRouter()
 
 class Location(BaseModel):
     city: str
@@ -25,24 +24,30 @@ class CreateGuideInput(BaseModel):
     visibility: str
     anonymous_allowed: bool
 
-class GetGuideInput(BaseModel):
+class GuideIdInput(BaseModel):
     id: str
 
-
-@app.get("/healthz")
-async def health_check():
-    return {"status": "ok"}
-
-@app.post("/save-guide")
+@guide_router.post("/save")
 async def save_guide(input: CreateGuideInput):   
     try:
         save_guide_usecase.exec(input)
+        return {"status": "ok"}
     except Exception as e:
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Erro ao salvar o guia")
     
-@app.get("/guide")
-async def get_guides(input: GetGuideInput):
+@guide_router.get("/")
+async def get_guides(input: GuideIdInput):
     guide_repository = GuideRepository()
     guide = guide_repository.get_guide(input.id) 
     return guide
+
+@guide_router.post("/delete")
+async def delete_guide(input: GuideIdInput):
+    try:
+        guide_repository = GuideRepository()
+        guide_repository.delete_guide(input.id) 
+        return {"status": "ok"}
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail="Erro ao deletar o guia")
