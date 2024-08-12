ad4ced4de24600f3f2573324badbbee1d8d7e76a
ju-vargas <ju.rodrigues0100@gmail.com>
2024-08-12 14:09:23 -0300

    #5 :lipstick: feat: implements guide page

--- guia-de/src/components/Section/Section.js
+++ guia-de/src/components/Section/Section.js
@@ -9,12 +9,12 @@ function Section({title, topics}) {
     <div className="section-container">
         <div className="header-container">
             <span className={`material-symbols-outlined`}>add_circle</span>
-            <h1 className={"stylized-title"}>{title}</h1>
+            <h1 className={"medium-stylized-title section-title"}>{title}</h1>
         </div>
         <div className="content-container">
             <div className="grid-container">
                 {topics && topics.length > 0 && topics.map((topic, index) => (
-                    <Topic key={index} title={topic.title} opinions={topic.opinions} childTopics={topic.childTopics} hierarchy={topic.hierarchy}/>
+                    <Topic key={index} title={topic.title} opinions={topic.opinions} childTopics={topic.childTopics} hierarchy={topic.hierarchy} topicId={topic.id}/>
                 ))}
             </div>
         </div>
