18fd277e9e63b42d8e1abc154a0a6a0fcbafd1ba
jpcorso <jp.corso123@gmail.com>
2024-08-12 10:58:11 -0300

    :sparkles: section update

--- python-server/src/usecases/update_section_usecase.py
+++ python-server/src/usecases/update_section_usecase.py
@@ -4,10 +4,12 @@ from infra.repository.section_repository import SectionRepository
 def exec(input):
 
     section = Section(
-        id=id,
+        id=input.id,
         title=input.title,
         topics=input.topics
     )
+
+    print(section)
 
     section_repository = SectionRepository()
     section_repository.update(section)
